6dff144893054c6a2e36c90a9f1864f6682d34d4
MdImran-Hossain <mdimranhossain.web@gamil.com>
2025-01-07 23:35:03 +0600

    call

---
+++ main.js
@@ -0,0 +1,215 @@
+function one(calback,v1,v2,v3,v4,v5){
+    setTimeout(()=>{
+        let value5=parseInt(v1+v2+v3+v4+v5)
+        console.log(`one number is:${value5}`);
+        calback(value5)
+    },2000)
+}
+function two(calback,v1,v2,v3,v4,v5,v6,v7){
+    setTimeout(()=>{
+        let value7=parseInt(v1+v2+v3+v4+v5+v6+v7)
+        console.log(`two number is:${value7}`);
+        calback(value7)
+    },3000)
+}
+function three(calback, v1,v2,v3){
+    setTimeout(()=>{
+        let value3=parseInt(v1+v2+v3)
+        console.log(`three number is:${value3}`);
+        calback(value3)
+    },200)
+}
+function four(calback,v1,v2,v3,v4,v5,v6,v7,v8){
+    setTimeout(()=>{
+        let value8=parseInt(v1+v2+v3+v4+v5+v6+v7+v8)
+        console.log(`four number is:${value8}`);
+        calback()
+    },2500)
+}
+function five(calback,v1){
+    setTimeout(()=>{
+        let value=parseInt(10+v1);
+        console.log(`five number is:${value}`);
+        calback(value)
+    },1000)
+}
+function six(calback,v1,v2,v3,v4,v5,v6){
+    setTimeout(()=>{
+        let value6=parseInt(v1+v2+v3+v4+v5+v6)
+        console.log(`six number is:${value6}`);
+        calback(value6)
+    },200)
+}
+function seven(calback,v2,v1){
+    setTimeout(()=>{
+        let value2=parseInt(v2+v1)
+        console.log(`seven number is:${value2}`);
+        calback(value2)
+    },1200)
+}
+function eight(calback,v1,v2,v3,v4){
+    setTimeout(()=>{
+        let value4=parseInt(v1+v2+v3+v4)
+        console.log(`eight number is:${value4}`);
+        calback(value4)
+    },300)
+}
+function nine(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function ten(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function eleven(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function twelve(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function thirteen(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function fourteen(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function fifteen(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function sixteen(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function seventeen(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function eighteen(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function nineteen(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function twenty(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function twentyOne(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function twenty2(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function twenty3(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function twenty4(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function twenty5(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function twenty6(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function twenty7(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function twenty8(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function twenty9(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+function thirty(calback){
+    setTimeout(()=>{
+        console.log(`eight number is:${value4}`);
+        calback()
+    },2500)
+}
+
+
+
+
+
+
+
+
+five((value)=>{
+    seven((value2)=>{
+        three((value3)=>{
+            eight((value4)=>{
+                one((value5)=>{
+                    six((value6)=>{
+                        two((value7)=>{
+                            four(()=>{
+
+                            },value,value2,value3,value4,value5,value6,value7,10)
+                            
+                        },value,value2,value3,value4,value5,value6,10)
+                        
+                    },value,value2,value3,value4,value5,10)
+                },value,value2,value3,value4,10)  
+            },value,value2,value3,10)
+        },value,value2,10)
+    },value,10)
+},10)
