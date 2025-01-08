9cac028e5872271790743f135c67a883ace5c576
MdImran-Hossain <mdimranhossain.web@gamil.com>
2025-01-08 23:30:56 +0600

    add

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ function one(calback,v1,v2,v3,v4,v5){
         let value5=parseInt(v1+v2+v3+v4+v5)
         console.log(`one number is:${value5}`);
         calback(value5)
-    },2000)
+    },300)
 }
 function two(calback,v1,v2,v3,v4,v5,v6,v7){
     setTimeout(()=>{
         let value7=parseInt(v1+v2+v3+v4+v5+v6+v7)
         console.log(`two number is:${value7}`);
         calback(value7)
-    },3000)
+    },600)
 }
 function three(calback, v1,v2,v3){
     setTimeout(()=>{
@@ -23,15 +23,15 @@ function four(calback,v1,v2,v3,v4,v5,v6,v7,v8){
     setTimeout(()=>{
         let value8=parseInt(v1+v2+v3+v4+v5+v6+v7+v8)
         console.log(`four number is:${value8}`);
-        calback()
-    },2500)
+        calback(value8)
+    },500)
 }
 function five(calback,v1){
     setTimeout(()=>{
         let value=parseInt(10+v1);
         console.log(`five number is:${value}`);
         calback(value)
-    },1000)
+    },100)
 }
 function six(calback,v1,v2,v3,v4,v5,v6){
     setTimeout(()=>{
@@ -45,7 +45,7 @@ function seven(calback,v2,v1){
         let value2=parseInt(v2+v1)
         console.log(`seven number is:${value2}`);
         calback(value2)
-    },1200)
+    },200)
 }
 function eight(calback,v1,v2,v3,v4){
     setTimeout(()=>{
@@ -54,137 +54,149 @@ function eight(calback,v1,v2,v3,v4){
         calback(value4)
     },300)
 }
-function nine(calback){
+function nine(calback,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10){
     setTimeout(()=>{
-        console.log(`eight number is:${value4}`);
-        calback()
-    },2500)
+        let value10=parseInt(v1+v2+v3+v4+v5+v6+v7+v8+v9+v10)
+        console.log(`nine number is:${value10}`);
+        calback(value10)
+    },500)
 }
-function ten(calback){
+function ten(calback,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14){
     setTimeout(()=>{
-        console.log(`eight number is:${value4}`);
-        calback()
-    },2500)
+        let value14=parseInt(v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14)
+        console.log(`ten number is:${value14}`);
+        calback(value14)
+    },500)
 }
-function eleven(calback){
+function eleven(calback,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14,v15,v16){
     setTimeout(()=>{
-        console.log(`eight number is:${value4}`);
-        calback()
-    },2500)
+        let value16=parseInt(v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16)
+        console.log(`eleven number is:${value16}`);
+        calback(value16)
+    },500)
 }
-function twelve(calback){
+function twelve(calback,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11){
     setTimeout(()=>{
-        console.log(`eight number is:${value4}`);
-        calback()
-    },2500)
+        let value11=parseInt(v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11)
+        console.log(`twelve number is:${value11}`);
+        calback(value11)
+    },500)
 }
 function thirteen(calback){
     setTimeout(()=>{
         console.log(`eight number is:${value4}`);
         calback()
-    },2500)
+    },500)
 }
 function fourteen(calback){
     setTimeout(()=>{
         console.log(`eight number is:${value4}`);
         calback()
-    },2500)
+    },500)
 }
-function fifteen(calback){
+function fifteen(calback,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14,v15,v16,v17,v18,v19,v20){
     setTimeout(()=>{
-        console.log(`eight number is:${value4}`);
-        calback()
-    },2500)
+        let value20=parseInt(v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17+v18+v19+v20)
+        console.log(`fifteen number is:${value20}`);
+        calback(value20)
+    },500)
 }
 function sixteen(calback){
     setTimeout(()=>{
         console.log(`eight number is:${value4}`);
         calback()
-    },2500)
+    },500)
 }
 function seventeen(calback){
     setTimeout(()=>{
         console.log(`eight number is:${value4}`);
         calback()
-    },2500)
+    },500)
 }
-function eighteen(calback){
+function eighteen(calback,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14,v15,v16,v17,v18){
     setTimeout(()=>{
-        console.log(`eight number is:${value4}`);
-        calback()
-    },2500)
+        let value18=parseInt(v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17+v18)
+        console.log(`eighteen number is:${value18}`);
+        calback(value18)
+    },500)
 }
 function nineteen(calback){
     setTimeout(()=>{
         console.log(`eight number is:${value4}`);
         calback()
-    },2500)
+    },500)
 }
-function twenty(calback){
+function twenty(calback,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12){
     setTimeout(()=>{
-        console.log(`eight number is:${value4}`);
-        calback()
-    },2500)
+        let value12=parseInt(v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12)
+        console.log(`twenty number is:${value12}`);
+        calback(value12)
+    },500)
 }
-function twentyOne(calback){
+function twentyOne(calback,v1,v2,v3,v4,v5,v6,v7,v8,v9){
     setTimeout(()=>{
-        console.log(`eight number is:${value4}`);
-        calback()
-    },2500)
+        let value9=parseInt(v1+v2+v3+v4+v5+v6+v7+v8+v9);
+        console.log(`twentyOne number is:${value9}`);
+        calback(value9)
+    },500)
 }
-function twenty2(calback){
+function twenty2(calback,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14,v15,v16,v17,v18,v19){
     setTimeout(()=>{
-        console.log(`eight number is:${value4}`);
-        calback()
-    },2500)
+        let value19=parseInt(v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17+v18+v19)
+        console.log(`twenty2 number is:${value19}`);
+        calback(value19)
+    },500)
 }
 function twenty3(calback){
     setTimeout(()=>{
         console.log(`eight number is:${value4}`);
         calback()
-    },2500)
+    },500)
 }
-function twenty4(calback){
+function twenty4(calback,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14,v15){
     setTimeout(()=>{
-        console.log(`eight number is:${value4}`);
-        calback()
-    },2500)
+        let value15=parseInt(v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15)
+        console.log(`twenty4 number is:${value15}`);
+        calback(value15)
+    },500)
 }
 function twenty5(calback){
     setTimeout(()=>{
         console.log(`eight number is:${value4}`);
         calback()
-    },2500)
+    },500)
 }
 function twenty6(calback){
     setTimeout(()=>{
         console.log(`eight number is:${value4}`);
         calback()
-    },2500)
+    },500)
 }
 function twenty7(calback){
     setTimeout(()=>{
         console.log(`eight number is:${value4}`);
         calback()
-    },2500)
+    },500)
 }
-function twenty8(calback){
+function twenty8(calback,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13){
     setTimeout(()=>{
-        console.log(`eight number is:${value4}`);
-        calback()
-    },2500)
+        let value13=parseInt(v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13)
+        console.log(`twenty8 number is:${value13}`);
+        calback(value13)
+    },500)
 }
-function twenty9(calback){
+function twenty9(calback,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14,v15,v16,v17){
     setTimeout(()=>{
-        console.log(`eight number is:${value4}`);
-        calback()
-    },2500)
+        let value17=parseInt(v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17)
+        console.log(`twenty9 number is:${value17}`);
+        calback(value17)
+    },500)
 }
 function thirty(calback){
     setTimeout(()=>{
         console.log(`eight number is:${value4}`);
         calback()
-    },2500)
+    },500)
 }
 
 
@@ -201,12 +213,34 @@ five((value)=>{
                 one((value5)=>{
                     six((value6)=>{
                         two((value7)=>{
-                            four(()=>{
-
+                            four((value8)=>{
+                                twentyOne((value9)=>{
+                                    nine((value10)=>{ 
+                                        twelve((value11)=>{
+                                           twenty((value12)=>{
+                                            twenty8((value13)=>{
+                                                ten((value14)=>{
+                                                    twenty4((value15)=>{
+                                                        eleven((value16)=>{
+                                                            twenty9((value17)=>{
+                                                                eighteen((value18)=>{
+                                                                    twenty2((value19)=>{
+                                                                        fifteen((value20)=>{
+                                                                            console.log(value20)
+                                                                        },value,value2,value3,value4,value5,value6,value7,value8,value9,value10,value11,value12,value13,value14,value15,value16,value17,value18,value19,10)
+                                                                    },value,value2,value3,value4,value5,value6,value7,value8,value9,value10,value11,value12,value13,value14,value15,value16,value17,value18,10)
+                                                                },value,value2,value3,value4,value5,value6,value7,value8,value9,value10,value11,value12,value13,value14,value15,value16,value17,10)
+                                                            },value,value2,value3,value4,value5,value6,value7,value8,value9,value10,value11,value12,value13,value14,value15,value16,10)
+                                                        },value,value2,value3,value4,value5,value6,value7,value8,value9,value10,value11,value12,value13,value14,value15,10)
+                                                    },value,value2,value3,value4,value5,value6,value7,value8,value9,value10,value11,value12,value13,value14,10)
+                                                },value,value2,value3,value4,value5,value6,value7,value8,value9,value10,value11,value12,value13,10)
+                                            },value,value2,value3,value4,value5,value6,value7,value8,value9,value10,value11,value12,10)
+                                           },value,value2,value3,value4,value5,value6,value7,value8,value9,value10,value11,10) 
+                                        },value,value2,value3,value4,value5,value6,value7,value8,value9,value10,10)
+                                    },value,value2,value3,value4,value5,value6,value7,value8,value9,10)
+                                },value,value2,value3,value4,value5,value6,value7,value8,10)
                             },value,value2,value3,value4,value5,value6,value7,10)
-                            
                         },value,value2,value3,value4,value5,value6,10)
-                        
                     },value,value2,value3,value4,value5,10)
                 },value,value2,value3,value4,10)  
             },value,value2,value3,10)
